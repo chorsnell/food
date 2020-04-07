cd0188f77f728dd0e015550fc2ad79c702da3715
Chris <5120660+poppahorse@users.noreply.github.com>
2020-04-07 18:02:49 +0100

    build: added cors deps + usage

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import path from "path";
 // services
 import routes from "./services";
@@ -7,12 +8,16 @@ import routes from "./services";
 const app = express();
 
 // config
+app.use(cors())
 app.use(express.static(path.join(__dirname, "public")));
 app.set("port", process.env.PORT || 3000);
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 // loop through app routes
 for (const route of routes) {
     const { method, path, handler } = route;
+    // add routes to express
     (app as any)[method](path, handler);
 }
 
