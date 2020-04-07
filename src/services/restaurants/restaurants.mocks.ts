9900aa06c0f2f8638007f0d65280fc9abca6dd26
Chris <5120660+poppahorse@users.noreply.github.com>
2020-04-07 17:07:57 +0100

    chore: added types to mocks

--- src/services/restaurants/restaurants.mocks.ts
+++ src/services/restaurants/restaurants.mocks.ts
@@ -1,14 +1,17 @@
+import { Restaurant, Restaurants } from "./restaurant.interface";
+
 /** sets defaults for install address mock */
 const getRestaurantDefault = () => ({
 	'id': 1,
 	'name': 'Godfathers',
 	'postCode': 'MK11 1AB',
-	'town': 'Milton Keynes'
+	'town': 'Milton Keynes',
+	'cuisine': 'Italian'
 });
 
 /** installation address mock that allows overwriting of specific properties with partials */
 /** For posterity, nice article on this here - https://itnext.io/mocking-in-typescript-a97267f7cea9 */
-export const getRestaurantMock = (a?: Partial<any>) => ({
+export const getRestaurantMock = (a?: Partial<Restaurant>): Restaurant => ({
 	...getRestaurantDefault(),
 	...a
 });
